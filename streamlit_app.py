34ecd91560c821fd1c97e53f9a9659fcc3365a97
mbkccad <mikeartz@hotmail.com>
2025-09-15 18:37:27 -0400

    Update streamlit_app.py
    deprecated code and added loc and iloc code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,8 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(my_dataframe.to_pandas(), use_container_width=True)
-#st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
                                                                       
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -29,19 +25,28 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
+ # New section to display smoothiefroot nutrition information
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    #st.write(ingredients_string)  
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+#if ingredients_list:
+ #   ingredients_string = ''
+
+  #  for fruit_chosen in ingredients_list:
+   #     ingredients_string += fruit_chosen + ' '
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
 
     time_to_insert = st.button('Submit Order')
 
@@ -50,11 +55,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
  # New section to display smoothiefroot nutrition information
-if ingredients_list:
-    ingredients_string = ''
+#if ingredients_list:
+ #   ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  #  for fruit_chosen in ingredients_list:
+   #     ingredients_string += fruit_chosen + ' '
+    #    st.subheader(fruit_chosen + 'Nutrition Information')
+     #   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+      #  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
