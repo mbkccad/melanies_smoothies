8c3b576dca6ae77dacb86cb86c9d305d7b996575
mbkccad <mikeartz@hotmail.com>
2025-09-15 14:47:27 -0400

    Update streamlit_app.py
    Fighting with Python indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
 if ingreients_list:
   ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '  
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '  
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
