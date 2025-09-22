52033339429ea6a255e83d62b6182e65d53d4806
Michael Kubacki <mikeartz@hotmail.com>
2025-09-22 21:48:07 +0000

    created packages.yml

--- models/Dim_State.sql
+++ models/Dim_State.sql
@@ -1,7 +1,15 @@
 {{ config(materialized='table') }}
 
+--select
+ --row_number() over (order by State_Name) as State_ID,
+ --State_Name,
+ --State_Abbreviation
+--from {{ ref('Stage_State') }}
+
 select
- row_number() over (order by State_Name) as State_ID,
+ {{ dbt_utils.generate_surrogate_key (
+    ['State_Name']
+ )}} as State_ID
  State_Name,
  State_Abbreviation
 from {{ ref('Stage_State') }}
