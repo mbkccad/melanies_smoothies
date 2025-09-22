93cf096cb193cb2da051ab0c8f5f979bacd93084
Michael Kubacki <mikeartz@hotmail.com>
2025-09-22 18:17:28 +0000

    test

--- models/Dim_State.sql
+++ models/Dim_State.sql
@@ -1,14 +1,13 @@
 
-
-
 {{ config(
     materialized='incremental',
-    unique_key='state'
+    unique_key='State_Name',
+    incremental_strategy='insert_overwrite',
+    on_schema_change='ignore'
 ) }}
 
 select
     State_Name,
     State_Abbreviation
 from {{ ref('Stage_State') }}
-
 
